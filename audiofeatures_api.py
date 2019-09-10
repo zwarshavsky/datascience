10c03857dfeaaae6aef13ee935421f8d096da3a4
Daniel <danielstarling4@gmail.com>
2019-09-10 10:10:21 -0700

    hiding api keys in config file

--- audiofeatures_api.py
+++ audiofeatures_api.py
@@ -9,7 +9,7 @@ from flask import (
 import requests
 import pandas as pd
 import json
-
+import config
 import pickle
 #import io
 import numpy as np
@@ -18,36 +18,41 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 # TODO Fix robots.txt
 app = Flask(__name__)
-client_credentials_manager = SpotifyClientCredentials(client_id='200f9f0be54b4daab1c2561098b3891a', client_secret='936688039db4402084f44eda09f17ffe')
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET)
 client_credentials_manager = client_credentials_manager
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-#@app.route("/api/", methods=['GET', 'POST'])
+# @app.route("/api/", methods=['GET', 'POST'])
 df = pd.read_csv('songmetadata_1.csv')
 array = df.values
 dfy = pd.read_csv('songtitles_1.csv')
 
+
 def cosine_similarity(a, b):
-  dot_product = np.dot(a,b)
-  norm_a = np.linalg.norm(a)
-  norm_b = np.linalg.norm(b)
-  return dot_product / (norm_a * norm_b)
-  #return np.sqrt(np.sum((a - b) ** 2))
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a * norm_b)
+    # return np.sqrt(np.sum((a - b) ** 2))
+
+
 def all_similarities(a, dfy):
-  similar_songs = []
-  for spotify_song, metadata in zip(array, dfy.values):
-    similarity = cosine_similarity(a, spotify_song)
-    similar_songs.append({'similarity': similarity, 'values': metadata})
-  return similar_songs
+    similar_songs = []
+    for spotify_song, metadata in zip(array, dfy.values):
+        similarity = cosine_similarity(a, spotify_song)
+        similar_songs.append({'similarity': similarity, 'values': metadata})
+    return similar_songs
 
 
 @app.route("/")
 def default():
     song = array[1549]
-
     similarities = all_similarities(song, dfy)
-    sorted_list = sorted(similarities, key=lambda i: i['similarity'], reverse=True)[1:51]
+    sorted_list = sorted(
+        similarities, key=lambda i: i['similarity'], reverse=True)[1:51]
     json_dict = {"songs": sorted_list}
     return str(json_dict)
+
 
 if __name__ == "__main__":
     app.run()
